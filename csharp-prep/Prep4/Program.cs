65f4bf4bec8fdb09c0423cbb6195adce51196131
NateTheUnavailable <nate@stevenogardner.com>
2024-06-15 12:36:44 -0600

    cnahges to Program to Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int> {};
+        int num = 1;
+        while (num != 0)
+        {
+            string response;
+            Console.WriteLine("Enter a number: ");
+            response = Console.ReadLine();
+            num = int.Parse(response);
+            numbers.Add(num);
+        }
+        int total = numbers.Sum();
+        int max = numbers.Max();
+        double average = numbers.Average();
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
