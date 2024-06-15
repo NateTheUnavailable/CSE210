eb40436ccc396165d6edcabd3027a9e534b92bfd
NateTheUnavailable <nate@stevenogardner.com>
2024-06-15 12:45:38 -0600

    changes to Program in Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int num;
+        double sq;
+        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Please enter your favorite number: ");
+        string me = Console.ReadLine();
+        num = int.Parse(me);
+        sq = num * num;
+        Console.WriteLine($"{name}, the square of your number is {sq}");
     }
 }
