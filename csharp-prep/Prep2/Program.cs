ff37039e73e021ca2e5ab9e11a3aee9ca1641da6
NateTheUnavailable <nate@stevenogardner.com>
2024-06-15 11:46:18 -0600

    changes to prep2 Program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int me = int.Parse(answer);
+        string letter = "";
+       if (me >= 90)
+        {
+            letter = "A";
+        }
+        else if (me >= 80)
+        {
+            letter = "B";
+        }
+        else if (me >= 70)
+        {
+            letter = "C";
+        }
+        else if (me >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your Grade Is {letter}");
+        if (me >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
