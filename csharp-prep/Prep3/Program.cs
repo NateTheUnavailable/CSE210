3369e2f7943df36aa10ea1b42d4260bfd8f7bb28
NateTheUnavailable <nate@stevenogardner.com>
2024-06-15 12:20:04 -0600

    changes to Program in Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,20);
+        int me = 0;
+        while (me != number)
+        {
+            string response;
+            Console.WriteLine("What is your guess?");
+            response = Console.ReadLine();
+            me = int.Parse(response);
+            if (me > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (me < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (me == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("That is not a number");
+            }
+        }
     }
 }
