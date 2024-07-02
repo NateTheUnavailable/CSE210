89d099de2eb180ba62b8a3b7a4d468e01f8d9f7a
NateTheUnavailable <nate@stevenogardner.com>
2024-07-02 11:17:22 -0600

    working homework assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Bob The Builder", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        MathAssignment a2 = new MathAssignment("Rob The Robber", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Writing a3 = new Writing("Jack Sparrow", "History", "The Golden Age of Piracy");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
